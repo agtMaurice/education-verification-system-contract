84fe780b7ffe70f342980cfc2fa9a09197021ee5
Colin Stark <collinscodes@gmail.com>
2023-05-02 19:49:27 +0100

    Update EducationCredentialVerification.sol

--- EducationCredentialVerification.sol
+++ EducationCredentialVerification.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+ // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -12,25 +12,17 @@ contract EducationCredentialVerification is ERC721, AccessControl {
 
     struct Credential {
         uint256 tokenId;
-        string institution;
-        string degree;
-        string major;
-        string studentName;
-        uint256 dateIssued;
-        bool isRevoked;
+        bytes32 institution;
+        bytes32 degree;
+        bytes32 major;
+        bytes32 studentName;
+        uint32 dateIssued;
     }
 
     mapping(uint256 => Credential) public credentials;
     Counters.Counter private _tokenIds;
 
-    event CredentialIssued(
-        uint256 indexed tokenId,
-        string studentName,
-        string degree,
-        string major,
-        string institution,
-        uint256 dateIssued
-    );
+    event CredentialIssued(uint256 indexed tokenId, bytes32 studentName, bytes32 degree, bytes32 major, bytes32 institution, uint32 dateIssued);
     event CredentialRevoked(uint256 indexed tokenId);
 
     constructor() ERC721("EducationCredential", "EDUC") {
@@ -38,11 +30,11 @@ contract EducationCredentialVerification is ERC721, AccessControl {
     }
 
     function issueCredential(
-        string memory institution,
-        string memory degree,
-        string memory major,
-        string memory studentName,
-        uint256 dateIssued,
+        bytes32 institution,
+        bytes32 degree,
+        bytes32 major,
+        bytes32 studentName,
+        uint32 dateIssued,
         address recipient
     ) public onlyRole(ISSUER_ROLE) {
         _tokenIds.increment();
@@ -55,57 +47,41 @@ contract EducationCredentialVerification is ERC721, AccessControl {
             degree,
             major,
             studentName,
-            dateIssued,
-            false
+            dateIssued
         );
 
         credentials[tokenId] = newCredential;
 
-        emit CredentialIssued(
-            tokenId,
-            studentName,
-            degree,
-            major,
-            institution,
-            dateIssued
-        );
+        emit CredentialIssued(tokenId, studentName, degree, major, institution, dateIssued);
     }
 
     function revokeCredential(uint256 tokenId) public onlyRole(ISSUER_ROLE) {
         require(_exists(tokenId), "Credential not found");
 
-        credentials[tokenId].isRevoked = true;
+        delete credentials[tokenId];
 
         emit CredentialRevoked(tokenId);
     }
 
     function isCredentialValid(uint256 tokenId) public view returns (bool) {
-        return _exists(tokenId) && !credentials[tokenId].isRevoked;
+        return _exists(tokenId) && credentials[tokenId].dateIssued != 0;
     }
 
-    function getCredential(
-        uint256 tokenId
-    ) public view returns (Credential memory) {
+    function getCredential(uint256 tokenId) public view returns (Credential memory) {
         require(_exists(tokenId), "Credential not found");
 
         return credentials[tokenId];
     }
 
-    function grantIssuerRole(
-        address account
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function grantIssuerRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
         grantRole(ISSUER_ROLE, account);
     }
 
-    function revokeIssuerRole(
-        address account
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function revokeIssuerRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
         revokeRole(ISSUER_ROLE, account);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
